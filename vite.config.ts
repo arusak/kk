66f312f3c8c77502b1ed05dddb1b686761aad6f7
Anton Rusak <anton.rusak@perfectart.com>
2025-01-07 08:31:36 +0100

    pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import {obfuscator} from 'rollup-obfuscator';
 // @ts-expect-error xxx
 import {readableCssModules} from 'vite-plugin-readable-css-modules';
+import {VitePWA} from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
@@ -13,10 +14,16 @@ export default defineConfig({
       target: 'browser',
       transform: 'base64',
     }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        theme_color: '#242424',
+      },
+    }),
   ],
   build: {
     sourcemap: false,
     outDir: 'build',
   },
-  base: '/kk/'
+  base: '/kk/',
 })
